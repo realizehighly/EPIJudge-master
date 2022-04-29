034e6aa8b828c2b0b6c78e368a22ed8a0a614cfe
realizehighly <realizehighly@naver.com>
2022-04-29 16:35:27 +0900

    4.1 패리티 계산하기(Computing the parity of a word)

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,11 +1,30 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class Parity {
+public class Parity { //4.1 패리티 계산하기
+//  @EpiTest(testDataFile = "parity.tsv")
+//  public static short parity(long x) { // 무식한 방법
+//    int numOfBitOne = 0;
+//    short parity = 0;
+//    while(x!=0){
+//      numOfBitOne += (x&1);
+//      x>>>=1;
+//    }
+//    if(numOfBitOne%2!=0)
+//      parity = 1;
+//    return parity;
+//  }
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    int numOfBitOne = 0;
+    short parity = 0;
+    while(x!=0){
+      numOfBitOne += (x&1);
+      x>>>=1;
+    }
+    if(numOfBitOne%2!=0)
+      parity = 1;
+    return parity;
   }
 
   public static void main(String[] args) {
