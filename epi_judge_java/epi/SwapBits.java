ccbce1220204b6f55fe3f8ca3b2c3f7b2d806c2e
realizehighly <realizehighly@naver.com>
2022-05-02 16:51:55 +0900

    문제 4.2 비트 스왑(Swap bits)

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -1,11 +1,14 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SwapBits {
+public class SwapBits { //문제 4.2 비트 스왑
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    if(((x>>>i)&1)!=((x>>>j)&1)){
+      long bitMask = (1L << i) | (1L << j);
+      x ^= bitMask;
+    }
+    return x;
   }
 
   public static void main(String[] args) {
